c5b13b5d4705d504540b0501d7c356333e421cfc
Aneesh Singh Rajoriya <aneeshsinghrajoriya@gmail.com>
2025-06-16 18:32:12 +0530

    Adding IDAStar Solver

---
+++ Solver/IDAstarSOLVER.h
@@ -0,0 +1,15 @@
+//
+// Created by anees on 28-06-2024.
+//
+#include "bits/stdc++.h"
+#include "..\\Models\genericRubiksCube.h"
+#ifndef RUBIKS_IDASTARSOLVER_H
+#define RUBIKS_IDASTARSOLVER_H
+
+template<typename T,typename H>
+
+class IDAstarSolver{
+    
+};
+
+#endif //RUBIKS_IDASTARSOLVER_H
--- main.cpp
+++ main.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include "bits/stdc++.h"
 //#include "C:\Users\anees\CLionProjects\RUbiks\Solver\BFSSOLVER.h"
-//#include "C:\Users\anees\CLionProjects\RUbiks\Solver\IDDFSSolver.h"
+#include "C:\Users\anees\CLionProjects\RUbiks\Solver\IDDFSSolver.h"
 //#include "C:\Users\anees\CLionProjects\RUbiks\Solver\DFSSOLVER.h"
 #include "C:\Users\anees\CLionProjects\RUbiks\Models\RubiksCube3darray.cpp"
 
 int main() {
 
 //    checking of IDDFSSolver
+    RubiksCube3darray cube;
+    cube.print();
+
+    vector<genericRubiksCube::MOVE> shuffle_moves = cube.randomShuffleColor(5);
+    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    cube.print();
+
+    IDDFSSolver<RubiksCube3darray, Hash3d> iddfsSolver(cube, 7);
+    vector<genericRubiksCube::MOVE> solve_moves = iddfsSolver.Solve();
+
+    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
+    cout << "\n";
+    iddfsSolver.rubiksCube.print();
+
+//
+//    checking for BFSSolver
+//
 //    RubiksCube3darray cube;
 //    cube.print();
 //
-//    vector<genericRubiksCube::MOVE> shuffle_moves = cube.randomShuffleColor(5);
-//    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    cube.print();
-//
-//    IDDFSSolver<RubiksCube3darray, Hash3d> iddfsSolver(cube, 7);
-//    vector<genericRubiksCube::MOVE> solve_moves = iddfsSolver.Solve();
-//
-//    for (auto move: solve_moves) cout << cube.getMove(move) << " ";
-//    cout << "\n";
-//    iddfsSolver.rubiksCube.print();
-
-
-//    checking of BFSSOlver
-
-//    RubiksCube3darray cube;
-//    cube.print();
-//
-//    vector<genericRubiksCube::MOVE> shuffle_moves = cube.randomShuffleColor(3);
+//    vector<genericRubiksCube::MOVE> shuffle_moves = cube.randomShuffleColor(6);
 //    for (auto move: shuffle_moves) cout << cube.getMove(move) << " ";
 //    cout << "\n";
 //    cube.print();
